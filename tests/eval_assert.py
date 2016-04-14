d63e69eb803fbcaf6f51ccc8db05819971aa8736
Joost Kremers <joostkremers@fastmail.fm>
2016-04-14 19:34:36 +0200

    Change AssertionError string in EvalAssert

--- tests/eval_assert.py
+++ tests/eval_assert.py
@@ -5,4 +5,5 @@ class EvalAssert():
     def assertEval(self, ast, env, expected):
         res = pymal.rep(ast, env)
         if res != expected:
-            raise AssertionError("eval failed: {} != {}".format(res, expected))
+            raise AssertionError("eval failed: expected '{}', got '{}'".
+                                 format(res, expected))
